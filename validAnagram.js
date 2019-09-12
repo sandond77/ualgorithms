8df4c74b8bb07cc4d5172c31f07f24817619c58a
sandond77 <sandu@ucdavis.edu>
2019-09-11 18:33:30 -0700

    added prompt for validAnagram.js

---
+++ validAnagram.js
@@ -0,0 +1,18 @@
+// Anagrams
+
+// Given two strings, write a function to determin if the second string is an anagram of the first.
+// An anagram is a word, phrase, or name formed by rearranging the letters of another such as cinema, formed from iceman.
+
+// Cases
+// ('','') returns true
+// ('aaz','zza') returns false
+// ('anagram', 'nagaram') returns true
+// ('rat','car') returns false
+// ('awesome','awesom') returns false
+// ('qwerty','qeywrt') returns true
+// ('texttwisttime','timetwisttext') returns true
+
+// Inputs will be:
+// 	single words
+// 	no puncutation, numbers, spaces
+// 	all lower case letters
