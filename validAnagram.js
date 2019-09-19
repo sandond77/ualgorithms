0dcc19a6047ca9f839f06bd0317e4ada512a48eb
sandond77 <sandu@ucdavis.edu>
2019-09-19 10:49:49 -0700

    finished validAnagram.js

--- validAnagram.js
+++ validAnagram.js
@@ -1,3 +1,5 @@
+
+
 // Anagrams
 
 // Given two strings, write a function to determin if the second string is an anagram of the first.
@@ -16,3 +18,40 @@
 // 	single words
 // 	no puncutation, numbers, spaces
 // 	all lower case letters
+
+function validAnagram(string1,string2){
+	if (string1.length != string2.length){
+		return false
+	};
+
+	let count1 = {};
+
+	for(let i = 0; i < string1.length; i++){
+		let letter = string1[i]
+
+		if(count1[letter]){
+			count1[letter]++
+		} else {
+			count1[letter] = 1
+		}
+	}
+
+	console.log(count1)
+
+	for(let i = 0; i < string2.length; i++){
+		let letter = string2[i];
+		if(!count1[letter]){
+			console.log('false')
+			return false;
+		} else {
+			count1[letter] -= 1
+		}
+	}
+
+	console.log(true)
+	return true
+
+}
+
+validAnagram('aaz','zza') 
+validAnagram('anagram', 'nagaram') 
