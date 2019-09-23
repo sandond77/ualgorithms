4f272ce7f2ba76c41abc041328555ea52aeb3d2b
sandond77 <sandu@ucdavis.edu>
2019-09-23 15:34:17 -0700

    added areThereDuplicates.js to Frequency counter and multiple pointers folders

---
+++ Frequency Counters/areThereDuplicates.js
@@ -0,0 +1,13 @@
+// Implement a function called, areThereDuplicates, which accepts a variable number of arguements, and checks whether thare are any duplicates among the arguments passed in.
+// You can solve this using the frequency counter pattern or multiple points pattern.
+
+// Restrictions - O(N) for space and time; Bonus - O(n log n) for time and O(1) for space
+
+// Given Examples
+// areThereDuplicates(1, 2, 3) // false;
+// areThereDuplicates(1, 2, 2) // true;
+// areThereDuplicates('a', 'b', 'c', 'a') // true
+
+function areThereDuplicates(){
+	
+}
