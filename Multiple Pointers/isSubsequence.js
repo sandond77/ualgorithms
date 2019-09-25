152c1d098a70865a091cde09c49b0e94ffd9d318
sandond77 <sandu@ucdavis.edu>
2019-09-25 11:11:06 -0700

    added my solution to isSubsequence.js

--- Multiple Pointers/isSubsequence.js
+++ Multiple Pointers/isSubsequence.js
@@ -10,6 +10,39 @@
 // isSubsequence('abc', 'abracadabra'); //true
 // isSubsequence('abc','acb'); //false (order matters)
 
-function isSubsequence(){
+// 1. Start left point on string1. Set right pointer to string2.
+// 2. Compare the two values, if there is no match. Move left pointer up string1. If there is a match, move pointer up string2 and continue comparison.
 
+
+function isSubsequence(){
+	let string1 = arguments[0];
+	let string2 = arguments[1];
+	let left = 0;
+	let right = 0;
+
+	while(left < string1.length){
+		console.log(string1[left],string2[right])
+		if(string1[left] === string2[right]){ //checks to see if letters are the same; we will move on to the next characters on both strings if there is a match
+			if(left === string1.length - 1){ //if we're at the end of string, we will return true because the substring exists
+				console.log(true);
+				return true;
+			}
+			left++; 
+			right++;
+		} else { //if there is no match, we will move onto the next character of string2
+			if(right  >= string2.length-1){ //this checks to see if we're at the end of string2; if we are, then we were unable to find all of our first string
+				console.log(false)
+				return false
+			}
+			right++
+		}
+	}
+
+	console.log(false)
+	return false
 }
+
+// isSubsequence('hello', 'hello world'); //true
+// isSubsequence('sing', 'sting'); //true
+// isSubsequence('abc', 'abracadabra'); //true
+isSubsequence('abc','acb'); //false (order matters)
