202e5188133f1cdb8b0546b8b51a01a8e82ba987
sandond77 <sandu@ucdavis.edu>
2019-09-25 10:40:36 -0700

    added my solution to averagePair.js

--- Multiple Pointers/averagePair.js
+++ Multiple Pointers/averagePair.js
@@ -4,6 +4,40 @@
 
 // Test Cases
 // averagePair([1,2,3], 2.5) // true
-// averagePair([1,3,3,5,6,7,10,12,19],8) // false
+// averagePair([1,3,3,5,6,7,10,12,19],8) // true
 // averagePair([-1,0,3,4,5,6], 4.1) //fale
 // averagePair([],4) //false
+
+
+function averagePair(){
+	let arr = arguments[0];
+	let target = arguments[1];
+
+	if(arr.length === 0){
+		console.log(false)
+		return false
+	}
+
+	let left = 0; 
+	let right = arr.length-1;
+
+	while (left < right){
+		let avg = (arr[left]+arr[right])/2;
+		if(avg === target){
+			console.log(true)
+			return true
+		} else if (avg > target){
+			right--
+		} else {
+			left ++
+		}
+	}
+
+	console.log(false)
+	return false
+}
+
+averagePair([1,2,3], 2.5) // true
+averagePair([1,3,3,5,6,7,10,12,19],8) // true
+averagePair([-1,0,3,4,5,6], 4.1) //fale
+averagePair([],4) //false 
