36345cd38394ef5236f5c5758429d467331ed0da
sandond77 <sandu@ucdavis.edu>
2019-09-25 11:12:47 -0700

    added given solution for averagePair.js

--- Multiple Pointers/averagePair.js
+++ Multiple Pointers/averagePair.js
@@ -41,3 +41,17 @@ averagePair([1,2,3], 2.5) // true
 averagePair([1,3,3,5,6,7,10,12,19],8) // true
 averagePair([-1,0,3,4,5,6], 4.1) //fale
 averagePair([],4) //false 
+
+
+// Given Solution
+// function averagePair(arr, num){
+//   let start = 0
+//   let end = arr.length-1;
+//   while(start < end){
+//     let avg = (arr[start]+arr[end]) / 2 
+//     if(avg === num) return true;
+//     else if(avg < num) start++
+//     else end--
+//   }
+//   return false;
+// }
