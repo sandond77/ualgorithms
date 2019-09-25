e7ebf654a9c3ee53da13e7d934bde6be75bdabfc
sandond77 <sandu@ucdavis.edu>
2019-09-25 10:09:31 -0700

    finished frequency counter version of areThereDuplicates.js

--- Frequency Counters/areThereDuplicates.js
+++ Frequency Counters/areThereDuplicates.js
@@ -9,5 +9,20 @@
 // areThereDuplicates('a', 'b', 'c', 'a') // true
 
 function areThereDuplicates(){
-	
+	let count={};
+	for(let i=0; i < arguments.length; i++){
+		// console.log(arguments[i])
+		if(!count[arguments[i]]){
+			count[arguments[i]] = 1
+		} else {
+			console.log(true)
+			return true
+		}
+	}
+	console.log(false)
+	return false
 }
+
+areThereDuplicates(1, 2, 3) // false;
+areThereDuplicates(1, 2, 2) // true;
+areThereDuplicates('a', 'b', 'c', 'a') // true
