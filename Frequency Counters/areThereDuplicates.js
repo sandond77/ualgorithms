7aca03ff1d4b8fc780b009c6cc2024576eb2ffcf
sandond77 <sandu@ucdavis.edu>
2019-09-25 10:26:44 -0700

    added solutions for both areThereDuplicate.js

--- Frequency Counters/areThereDuplicates.js
+++ Frequency Counters/areThereDuplicates.js
@@ -26,3 +26,19 @@ function areThereDuplicates(){
 areThereDuplicates(1, 2, 3) // false;
 areThereDuplicates(1, 2, 2) // true;
 areThereDuplicates('a', 'b', 'c', 'a') // true
+
+// Given Solutions
+// function areThereDuplicates() {
+// 	let collection = {}
+// 	for(let val in arguments){
+// 	  collection[arguments[val]] = (collection[arguments[val]] || 0) + 1
+// 	}
+// 	for(let key in collection){
+// 	  if(collection[key] > 1) return true
+// 	}
+// 	return false;
+//   }
+
+//   function areThereDuplicates() {
+// 	return new Set(arguments).size !== arguments.length;
+//   }
--- Multiple Pointers/areThereDuplicates.js
+++ Multiple Pointers/areThereDuplicates.js
@@ -27,3 +27,23 @@ function areThereDuplicates(){
 areThereDuplicates(1, 2, 3) // false;
 areThereDuplicates(1, 2, 2) // true;
 areThereDuplicates('a', 'b', 'c', 'a') // true
+
+// Given Solutions
+// function areThereDuplicates(...args) {
+// 	// Two pointers
+// 	args.sort((a,b) => a > b);
+// 	let start = 0;
+// 	let next = 1;
+// 	while(next < args.length){
+// 	  if(args[start] === args[next]){
+// 		  return true
+// 	  }
+// 	  start++
+// 	  next++
+// 	}
+// 	return false
+// }
+
+// function areThereDuplicates() {
+// 	return new Set(arguments).size !== arguments.length;
+//   }
