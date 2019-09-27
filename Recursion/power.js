357b22403d140d8e47fbaab4554329bb962d8e25
sandond77 <sandu@ucdavis.edu>
2019-09-27 15:43:21 -0700

    added my solution to power.js

--- Recursion/power.js
+++ Recursion/power.js
@@ -3,8 +3,10 @@
 // Test Cases 
 // power(2,0) // 1
 // power(2,2) // 4
-// power(2,2) // 6
+// power(2,4) // 16
 
-function power(){
-	
+function power(base,exponent){
+	if(exponent === 0 ) return 1;
+	return (base * power(base,exponent - 1))
 }
+
