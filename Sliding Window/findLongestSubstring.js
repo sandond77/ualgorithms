3b51dccc896fa47c88b289ae4e8b0c6bd3bf2626
sandond77 <sandu@ucdavis.edu>
2019-09-26 22:41:58 -0700

    added solution for findLongestSubstring.js

--- Sliding Window/findLongestSubstring.js
+++ Sliding Window/findLongestSubstring.js
@@ -10,10 +10,34 @@
 // findLongestSubstring('longestsubstring') //8
 // findLongestSubstring('thisishowedoit) //6
 
-1. Create an object for seen characters. 
-2. Loop through string. Check object to see if character has been seen yet
-3. If a repeated characer appears, close the 
+// 1. Create an object for seen characters. 
+// 2. Loop through string. Check object to see if character has been seen yet
+// 3. If a repeated characer appears, close the 
 
 
-function findLongestSubstring(){
-}
+// function findLongestSubstring(){
+// 	let characters = {};
+
+// }
+
+function findLongestSubstring(str) {
+	let longest = 0;
+	let seen = {};
+	let start = 0;
+   
+	for (let i = 0; i < str.length; i++) {
+	  let char = str[i];
+	  if (seen[char]) {
+		start = Math.max(start, seen[char]);
+		console.log(char, seen[char], start)
+	  }
+	  // index - beginning of substring + 1 (to include current in count)
+	  longest = Math.max(longest, i - start + 1);
+	  console.log('letter:'+char, 'length:'+longest)
+	  // store the index of the next char so as to not double count
+	  seen[char] = i + 1;
+	}
+	return longest;
+  }
+
+  findLongestSubstring('thisisawesome') //6
