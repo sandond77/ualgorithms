04749d6956610e0d6d25e40a06a6863153b60698
sandond77 <sandu@ucdavis.edu>
2019-09-28 15:35:00 -0700

    added reverse.js

---
+++ Recursion/reverse.js
@@ -0,0 +1,8 @@
+Write a recursive function called reverse which accepts a string and returns a new string in reverse.
+
+reverse('awesome') // emosewa
+reverse('rithmschool') // loohchsmtir
+
+function reverse(){
+	
+}
