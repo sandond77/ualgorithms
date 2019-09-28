0536f810ebe4334accfc1e19c4470af8ceb4ee8a
sandond77 <sandu@ucdavis.edu>
2019-09-28 14:56:43 -0700

    completed factorial.js

---
+++ Recursion/factorial.js
@@ -0,0 +1,13 @@
+Write a function factorial which accepts a number and returns the fatorial of that number. A factorial is the product of an integer and all the integers below it. 
+
+// 4! = 24
+// 0! = 1
+//factorial(1) // 1
+// factorial(2) // 2
+// factorial(4) // 24
+// factorial(7) // 5040
+
+function factorial(num){
+	if(num === 0) return 1;
+	return num*(factorial(num-1));
+}
