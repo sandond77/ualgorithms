2f09061382f8a7f81d5ab441823e80a645a9f071
sandond77 <sandu@ucdavis.edu>
2019-09-28 15:07:19 -0700

    added my solution for recursiveRange.js

--- Recursion/recursiveRange.js
+++ Recursion/recursiveRange.js
@@ -3,6 +3,8 @@
 // recursiveRange(6) // 21
 // recursiveRange(10)// 55
 
-function recursiveRange(){
-	
+function recursiveRange(num){
+	if(num===1) return 1;
+
+	return num + recursiveRange(num-1)
 }
