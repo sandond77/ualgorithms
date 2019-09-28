813511c43045f35c06f1ed9393bb2f5ebedc5633
sandond77 <sandu@ucdavis.edu>
2019-09-28 15:02:17 -0700

    added my solution for productOfArray.js

--- Recursion/productOfArray.js
+++ Recursion/productOfArray.js
@@ -3,6 +3,9 @@
 // productOfArray([1,2,3]) // 6
 // productOfArray([1,2,3,10]) //60
 
-function productOfArray(){
-
+function productOfArray(arr){
+	if (arr.length === 1) return arr[0]
+	return arr[0] * productOfArray(arr.slice(1))
 }
+
+productOfArray([1,2,3]) // 6
