0ea14b3aa228f20e1410cd1b886d9fb53289b014
sandond77 <sandu@ucdavis.edu>
2019-10-01 20:24:21 -0700

    added my solution for reverse.js

--- Recursion/reverse.js
+++ Recursion/reverse.js
@@ -1,8 +1,11 @@
-Write a recursive function called reverse which accepts a string and returns a new string in reverse.
+// Write a recursive function called reverse which accepts a string and returns a new string in reverse.
+
+// reverse('awesome') // emosewa
+// reverse('rithmschool') // loohchsmtir
+
+function reverse(str){
+	if (str.slice(1) === "") return str;
+	return str.slice(1) + str[0] //This works because of FIFO and how functions will resolve
+} 
 
 reverse('awesome') // emosewa
-reverse('rithmschool') // loohchsmtir
-
-function reverse(){
-	
-}
