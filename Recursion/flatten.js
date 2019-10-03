010c50b51b4a0493696663ac72082a8e6825af06
sandond77 <sandu@ucdavis.edu>
2019-10-02 22:27:46 -0700

    addded flatten.js

---
+++ Recursion/flatten.js
@@ -0,0 +1,11 @@
+// Write a recursive function called flatten which accepts an array of strings and returns a new array of all values flattened.
+
+// Test Cases 
+// flatten([1,2,3, [4,5]]) //[1,2,3,4,5]
+// flatten([1,[2, [3, 4], [[5]]]]) //[1,2,3,4,5]
+// flatten([[1],[2],[3]]) //[1,2,3]
+// flatten([[[[1], [[[2]]], [[[[[[3]]]]]]]]]) //[1,2,3]
+
+function flatten(){
+
+}
