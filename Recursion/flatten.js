bcc2e7ef53f81f269f480207819d0eb39e8aa5ec
sandond77 <sandu@ucdavis.edu>
2019-10-02 22:38:30 -0700

    added solution for flatten.js

--- Recursion/flatten.js
+++ Recursion/flatten.js
@@ -6,6 +6,14 @@
 // flatten([[1],[2],[3]]) //[1,2,3]
 // flatten([[[[1], [[[2]]], [[[[[[3]]]]]]]]]) //[1,2,3]
 
-function flatten(){
-
+function flatten(arr){
+	let newArr = [];
+	for(let i=0; i<arr.length; i++){
+		if(Array.isArray(arr[i])){
+			newArr = newArr.concat(flatten(arr[i]))
+		} else {
+			newArr.push(arr[i])
+		}
+	}
+	return newArr
 }
