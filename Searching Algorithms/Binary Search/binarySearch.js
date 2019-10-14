73b013f8add86862d6d1c728c2d9f31f713d2c9f
sandond77 <sandu@ucdavis.edu>
2019-10-13 22:48:08 -0700

    added binarySearch.js

---
+++ Searching Algorithms/Binary Search/binarySearch.js
@@ -0,0 +1,14 @@
+// Write a function called binarySearch which accepts a sorted array and a value and returns the index at which the value exists. Otherwise return -1.
+
+// Test Cases
+// binarySearch([1,2,3,4,5], 2) //1
+// binarySearch([1,2,3,4,5],3) // 2
+// binarySearch([1,2,3,4,5],5) // 4
+// binarySearch([1,2,3,4,5],6) // -1
+// binarySearch([5, 6, 10, 13, 14, 18, 30, 34, 35, 37, 40, 44, 64, 79, 84, 86, 95, 96, 98, 99],10) // 2
+// binarySearch([5, 6, 10, 13, 14, 18, 30, 34, 35, 37, 40, 44, 64, 79, 84, 86, 95, 96, 98, 99],95) // 16
+// binarySearch([5, 6, 10, 13, 14, 18, 30, 34, 35, 37, 40, 44, 64, 79, 84, 86, 95, 96, 98, 99],100) // -1
+
+function binarySearch(){
+	
+}
--- Searching Algorithms/Linear Search/linearSearch.js
+++ Searching Algorithms/Linear Search/linearSearch.js
@@ -15,11 +15,9 @@
 function linearSearch(arr, num){
 	for(let i=0; i < arr.length; i++){
 		if (arr[i]===num){
-			console.log(i)
 			return i
 		}
 	}
-	console.log(-1)
 	return -1
 }
 
