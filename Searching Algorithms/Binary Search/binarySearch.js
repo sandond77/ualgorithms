f29edef0ca0c7e2ba21277c9c421d6e876caa493
sandond77 <sandu@ucdavis.edu>
2019-10-16 21:07:59 -0700

    added psuedocode for binarySearch.js

--- Searching Algorithms/Binary Search/binarySearch.js
+++ Searching Algorithms/Binary Search/binarySearch.js
@@ -1,3 +1,5 @@
+import { element, array } from "prop-types";
+
 // Write a function called binarySearch which accepts a sorted array and a value and returns the index at which the value exists. Otherwise return -1.
 
 // Test Cases
@@ -10,5 +12,13 @@
 // binarySearch([5, 6, 10, 13, 14, 18, 30, 34, 35, 37, 40, 44, 64, 79, 84, 86, 95, 96, 98, 99],100) // -1
 
 function binarySearch(){
-	
+
 }
+
+// Pseudocode
+// 1. Left starts at first element. Right starts at last element. Mid point is array.length/2. 
+// 2. Compare midpoint value to desired value.
+// 	a. If Midpoint > value, set old midpoint as right. New mid point will be new array length/2. 
+// 	b. If Midpoint < value, set old midpoint as left. New midpoint will be new array length/2.
+// 3. Repeat until value is found and return the index. 
+// 	a. Return -1 if value is not found.
