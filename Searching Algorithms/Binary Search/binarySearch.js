ef5c9470f38c4e1ed10ff43cd7de41c9c67a6589
sandond77 <sandu@ucdavis.edu>
2019-10-26 09:15:53 -0700

    added and tested my solution for binary search

--- Searching Algorithms/Binary Search/binarySearch.js
+++ Searching Algorithms/Binary Search/binarySearch.js
@@ -1,4 +1,3 @@
-import { element, array } from "prop-types";
 
 // Write a function called binarySearch which accepts a sorted array and a value and returns the index at which the value exists. Otherwise return -1.
 
@@ -11,8 +10,39 @@ import { element, array } from "prop-types";
 // binarySearch([5, 6, 10, 13, 14, 18, 30, 34, 35, 37, 40, 44, 64, 79, 84, 86, 95, 96, 98, 99],95) // 16
 // binarySearch([5, 6, 10, 13, 14, 18, 30, 34, 35, 37, 40, 44, 64, 79, 84, 86, 95, 96, 98, 99],100) // -1
 
-function binarySearch(){
+function binarySearch(arr, value){
+	let start = 0;
+	let end = arr.length - 1;
+	let mid =  Math.ceil((arr.length -1)/2);
 
+	// console.log(start, mid, end)
+	while (end > start){
+		// console.log(start, mid, end)
+		// console.log(arr)
+		if(arr[mid] > value){
+			end = mid;
+			let temp = arr.slice(start,end+1); //creating a temporary array from the new range which we can use to calculate a new mid point
+			let add =  Math.ceil((temp.length -1)/2); //finding the value of the length between start and the new mid point of the new array 
+			mid = start + add; //adding the mid length to the start to find the new mid point
+		}
+
+		if(arr[mid] < value){
+			start = mid;
+			end = arr.length -1;
+			let temp = arr.slice(start,end+1);
+			let add =  Math.ceil((temp.length -1)/2);
+			mid = start + add
+		}
+
+		if(arr[mid] === value){
+			// console.log(arr[mid])
+			console.log(mid)
+			return mid
+		}
+	} 
+
+	console.log(false + " -1")
+	return -1
 }
 
 // Pseudocode
@@ -22,3 +52,11 @@ function binarySearch(){
 // 	b. If Midpoint < value, set old midpoint as left. New midpoint will be new array length/2.
 // 3. Repeat until value is found and return the index. 
 // 	a. Return -1 if value is not found.
+
+binarySearch([1,2,3,4,5], 2) //1
+binarySearch([1,2,3,4,5],3) // 2
+binarySearch([1,2,3,4,5],5) // 4
+binarySearch([1,2,3,4,5],6) // -1
+binarySearch([5, 6, 10, 13, 14, 18, 30, 34, 35, 37, 40, 44, 64, 79, 84, 86, 95, 96, 98, 99],10) // 2
+binarySearch([5, 6, 10, 13, 14, 18, 30, 34, 35, 37, 40, 44, 64, 79, 84, 86, 95, 96, 98, 99],95) // 16
+binarySearch([5, 6, 10, 13, 14, 18, 30, 34, 35, 37, 40, 44, 64, 79, 84, 86, 95, 96, 98, 99],100) // -1
