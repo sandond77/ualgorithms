2b24e91001cf41d17e3812c08da13b07955fe259
sandond77 <sandu@ucdavis.edu>
2019-10-26 10:04:15 -0700

    added solution to binarySearch.js

--- Searching Algorithms/Binary Search/binarySearch.js
+++ Searching Algorithms/Binary Search/binarySearch.js
@@ -60,3 +60,16 @@ binarySearch([1,2,3,4,5],6) // -1
 binarySearch([5, 6, 10, 13, 14, 18, 30, 34, 35, 37, 40, 44, 64, 79, 84, 86, 95, 96, 98, 99],10) // 2
 binarySearch([5, 6, 10, 13, 14, 18, 30, 34, 35, 37, 40, 44, 64, 79, 84, 86, 95, 96, 98, 99],95) // 16
 binarySearch([5, 6, 10, 13, 14, 18, 30, 34, 35, 37, 40, 44, 64, 79, 84, 86, 95, 96, 98, 99],100) // -1
+
+// Given Solution
+function binarySearch(arr, elem) {
+    var start = 0;
+    var end = arr.length - 1;
+    var middle = Math.floor((start + end) / 2);
+    while(arr[middle] !== elem && start <= end) {
+        if(elem < arr[middle]) end = middle - 1;
+        else start = middle + 1;
+        middle = Math.floor((start + end) / 2);
+    }
+    return arr[middle] === elem ? middle : -1;
+}
