b7ec49a441531f93abb33bb1119e0aecff418c00
sandond77 <sandu@ucdavis.edu>
2019-10-28 10:13:53 -0700

    added test cases for naiveStringSearch

--- Searching Algorithms/Naive String Search/naiveStringSearch.js
+++ Searching Algorithms/Naive String Search/naiveStringSearch.js
@@ -2,14 +2,14 @@ function naiveStringSearch(long, short){
 	let counter = 0;
 	for (let i=0; i < long.length; i++){
 		for(let j=0; j < short.length; j++){
-			console.log(long[i+j],short[j])
+			// console.log(long[i+j],short[j])
 			if(short[j] !== long[i+j]){
-				console.log('break')
+				// console.log('break')
 				break
 			}
 
 			if (j === short.length - 1){
-				console.log('found one')
+				// console.log('found one')
 				counter++
 			}
 		}
@@ -18,4 +18,6 @@ function naiveStringSearch(long, short){
 	return counter
 }
 
-naiveStringSearch('lorie loled', 'lol')
+naiveStringSearch('lorie loled', 'lol') // returns 1
+naiveStringSearch('lorie loled', 'lo') // returns 2
+naiveStringSearch('lorie loled', 'go') // returns 0
