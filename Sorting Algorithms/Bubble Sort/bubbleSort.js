7cb1d01e555e45c4d5bd61c30028d98f3a42c2a2
sandond77 <sandu@ucdavis.edu>
2019-10-28 11:14:49 -0700

    added my solution to bubbleSort.js;

--- Sorting Algorithms/Bubble Sort/bubbleSort.js
+++ Sorting Algorithms/Bubble Sort/bubbleSort.js
@@ -5,5 +5,19 @@
 // return the sorted array
 
 function bubbleSort(arr){
-	
+	for(let i=arr.length-1; i > 0; i--){ //this loop counts from end to beginning and lets us know to stop sorting the largest value once it reaches the end
+		for(let j=0; j<= i-1; j++){ //this loop moves forward in the array and will stop until it reaches i, which should be at the start of the sorted values 
+			if(arr[j] > arr[j+1]){ //comparing the current index to the next index to see which one is larger; swap if larger value is first
+				let temp = arr[j];
+				arr[j] = arr[j+1];
+				arr[j+1] = temp
+				console.log(arr)
+			}
+		}
+	}
+	console.log(arr)
 }
+
+// bubbleSort([3,4,1,2]) // [1,2,3,4]
+// bubbleSort([1,10,2,15,30,5,33]) //[1,2,5,10,15,30,33]
+// bubbleSort([5,1,4,2,8]) //[1,2,4,5,8]
