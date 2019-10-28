fd0bbaf8e8547cd1fa9424bca1c5600b18c74441
sandond77 <sandu@ucdavis.edu>
2019-10-28 10:58:19 -0700

    added base code for bubbleSort.js

--- Searching Algorithms/Naive String Search/naiveStringSearch.js
+++ Searching Algorithms/Naive String Search/naiveStringSearch.js
@@ -2,16 +2,8 @@ function naiveStringSearch(long, short){
 	let counter = 0;
 	for (let i=0; i < long.length; i++){
 		for(let j=0; j < short.length; j++){
-			// console.log(long[i+j],short[j])
-			if(short[j] !== long[i+j]){
-				// console.log('break')
-				break
-			}
-
-			if (j === short.length - 1){
-				// console.log('found one')
-				counter++
-			}
+			if(short[j] !== long[i+j]) break
+			if (j === short.length - 1) counter++
 		}
 	}
 	console.log(counter)
---
+++ Sorting Algorithms/Bubble Sort/bubbleSort.js
@@ -0,0 +1,9 @@
+// Pseudocode
+// Start looping from a variable called i from the end of the array towards the beginning
+// Start an inner loop with a variable called j from the beginning until i-1
+// if arr[j] is greater than arr[j+1], swap the two values
+// return the sorted array
+
+function bubbleSort(arr){
+	
+}
