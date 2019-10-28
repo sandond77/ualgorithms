828fc97dff5d5d6be3250d94a65592c9ac321de9
sandond77 <sandu@ucdavis.edu>
2019-10-28 14:14:24 -0700

    added given solutions for bubblesort.js

--- Sorting Algorithms/Bubble Sort/bubbleSort.js
+++ Sorting Algorithms/Bubble Sort/bubbleSort.js
@@ -6,7 +6,7 @@
 
 function bubbleSort(arr){
 	for(let i=arr.length-1; i > 0; i--){ //this loop counts from end to beginning and lets us know to stop sorting the largest value once it reaches the end
-		for(let j=0; j<= i-1; j++){ //this loop moves forward in the array and will stop until it reaches i, which should be at the start of the sorted values 
+		for(let j=0; j<= i-1; j++){ //this loop moves forward in the array and will stop until it reaches i, which should be at the start of the sorted/largest values 
 			if(arr[j] > arr[j+1]){ //comparing the current index to the next index to see which one is larger; swap if larger value is first
 				let temp = arr[j];
 				arr[j] = arr[j+1];
@@ -21,3 +21,33 @@ function bubbleSort(arr){
 // bubbleSort([3,4,1,2]) // [1,2,3,4]
 // bubbleSort([1,10,2,15,30,5,33]) //[1,2,5,10,15,30,33]
 // bubbleSort([5,1,4,2,8]) //[1,2,4,5,8]
+bubbleSort([37,45,29,8])
+
+// Given Solution using ES5
+// function bubbleSort(arr){
+// 	for(let i=arr.length; i > 0; i--){
+// 		console.log(arr, arr[j], arr[j+1])
+// 		if(arr[j] > arr[j+1]){
+// 			let temp = arr[j];
+// 			arr[j] = arr[j+1];
+// 			arr[j+1] = temp;
+// 		}
+// 	}
+// 	return arr
+// }
+
+// Given Solution Using ES6/ES2015
+
+// Given Solution using ES5
+// function bubbleSort(arr){
+	// const swap = (arr,index1, index2) => {
+	// 	[arr[index1], arr[index2]] = [arr[index2],arr[index1]];
+	// }
+// 	for(let i=arr.length; i > 0; i--){
+// 		console.log(arr, arr[j], arr[j+1])
+// 		if(arr[j] > arr[j+1]){
+// 			swap(arr, j, j+1)
+// 		}
+// 	}
+// 	return arr
+// }
