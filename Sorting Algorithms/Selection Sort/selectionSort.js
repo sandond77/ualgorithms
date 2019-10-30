93d77aad68e821db76afe3701afaf44c242ec2a4
sandond77 <sandu@ucdavis.edu>
2019-10-30 09:59:37 -0700

    adde selection sort with solutions

--- Sorting Algorithms/Selection Sort/selectionSort.js
+++ Sorting Algorithms/Selection Sort/selectionSort.js
@@ -14,20 +14,63 @@ function selectionSort(arr){
 	}
 	
 	for(let i=0; i < arr.length-1; i++){ //first loop will go through our numbers and prevent us from searching through the sorted portion of the array
-		let lowest = arr[i]  //setting the initial number as the smallest value
 		let index;
+		let flag = false;
 		for(let j=i; j < arr.length -1 ; j++){ //second loop will allow us to compare the two values to see which one is smallest
+			let lowest = arr[j] //setting the initial number as the smallest value
 			index = j; //setting the initial index to j incase the starting value is the smallest
 			console.log(j,j+1, arr[j], arr[j+1])
 			if(arr[j] > arr[j+1]){ //if the current value is larger than the next value, we will set lowest as the new smaller value and change our saved index position as well
 				lowest = arr[j+1];
-				index = j+1
+				index = j+1;
+				flag = true;
 			}
 			
 		}
-		swap(arr, i, index); //swapping the start of the sort with the smallest value we found
+		if(flag){
+			swap(arr, i, index); //swapping the start of the sort with the smallest value we found
+		}
+
 		console.log(arr);
 	}
 }
  
 selectionSort([5,3,4,1,2])
+
+
+// Given Solution using ES5
+function selectionSort(arr){
+	for(let i=0; i<arr.length; i++){
+		let lowest = i;
+		for(let j=i+1; j<arr.length; j++){
+			if(arr[j] < arr[lowest]){
+				lowest = j;
+			}
+		}
+
+		if( i!== lowest){
+			let temp = arr[i];
+			arr[i] = arr[lowest];
+			arr[lowest] = temp;
+		}
+	}
+	return arr
+}
+ 
+selectionSort([5,3,4,1,2])
+
+//Given Solution using es6/es2015
+function selectionSort(arr){
+	const swap = (arr, idx1, idx2) => ([arr[idx1], arr[idx2]] = [arr[idx2],arr[idx1]]);
+	
+	for(let i=0; i<arr.length; i++){
+		let lowest = i;
+		for(let j=i+1; j<arr.length; j++){
+			if(arr[j] < arr[lowest]){
+				lowest = j;
+			}
+		}
+		if(i!==lowest) swap(arr, i, lowest)
+	}
+	return arr
+}
