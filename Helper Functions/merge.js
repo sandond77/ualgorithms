7617dd2fcff6279d3eaaae455efd21da88889252
sandond77 <sandu@ucdavis.edu>
2019-10-31 10:49:58 -0700

    created new folder for helper functions

--- Helper Functions/merge.js
+++ Helper Functions/merge.js
@@ -38,33 +38,33 @@
 // 	return results
 // }
 // merge([1,3,9,50,80], [2,6,11])
-merge([1,10,50], [2,14,99,100])
+// merge([1,10,50], [2,14,99,100])
 
 
 // Given Solution
-// function merge(arr1,arr2){
-// 	let results = [];
-// 	let i = 0;
-// 	let j = 0;
+function merge(arr1,arr2){
+	let results = [];
+	let i = 0;
+	let j = 0;
 
-// 	while(i < arr1.length && j < arr2.length){
-// 		if(arr2[j]> arr1[i]){
-// 			results.push(arr1[i]);
-// 			i++;
-// 		} else {
-// 			results.push(arr2[j]);
-// 			j++
-// 		}
-// 	}
+	while(i < arr1.length && j < arr2.length){
+		if(arr2[j]> arr1[i]){
+			results.push(arr1[i]);
+			i++;
+		} else {
+			results.push(arr2[j]);
+			j++
+		}
+	}
 
-// 	while(i < arr1.length){
-// 		results.push(arr1[i]);
-// 		i++
-// 	}
+	while(i < arr1.length){
+		results.push(arr1[i]);
+		i++
+	}
 
-// 	while(j < arr2.length){
-// 		results.push(arr2[j]);
-// 		j++
-// 	}
-// 	return results
-// }
+	while(j < arr2.length){
+		results.push(arr2[j]);
+		j++
+	}
+	return results
+}
---
+++ Helper Functions/swap.js
@@ -0,0 +1,9 @@
+function swap(arr, index1, index2){
+    let temp = arr[index1];
+    arr[index1] = arr[index2];
+    arr[index2] = temp;
+}
+
+const swap = (arr, index1, index2){
+    [arr[index1], arr[index2]] = [arr[index2], arr[index2]];
+}
