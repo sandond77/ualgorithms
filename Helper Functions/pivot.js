788faeb74596ea7857f7be968c623605bec69888
sandond77 <sandu@ucdavis.edu>
2019-11-03 14:17:39 -0800

    added given solution for pivot.js

--- Helper Functions/pivot.js
+++ Helper Functions/pivot.js
@@ -18,13 +18,38 @@ function pivot (arr, start=0, end=arr.length-1){
 	let pivotIndex = 0; 
 	let pivot = arr[start];
 	for (let i=1; i <= end; i++){
+		console.log(arr);
 		if (pivot > arr[i]){	//comparison is always being made with the pivot number but it does not move til the end
 			pivotIndex++; //this pivot index increment will tell us where to switch the pivot number at the end
 			swap(arr, i, pivotIndex); //as we compare values, we want to continue to move the smaller values to the start of the array/ left of the pivt
 		}
-	}
+	}	
+	console.log(pivotIndex)
 	swap(arr, start, pivotIndex); //moves out pivot number to the correct place based on the pivot index counter
+	console.log(arr)
+
 	return pivotIndex
 }
 
-pivot([28,41,4,11,16,1,40,14,36,37,42,18])
+pivot([28,41,4,11,16,1,40,14,36,37,42,18]) //should return 6
+pivot([4,8,2,1,5,7,6,3]) //should return 3
+
+
+// GIVEN SOLUTION
+function pivot(arr, start=0, end=arr.length+1){
+	let pivot = arr[start];
+	let swapIdx = start;
+	
+	for(let i = start+1; i<arr.length; i++){
+		if(pivot > arr[i]){
+			swapIdx++;
+			swap(arr, swapIdx, i);
+		}
+	}
+	swap(arr, start, swapIdx);
+	return swapIdx
+}
+
+
+pivot([28,41,4,11,16,1,40,14,36,37,42,18]) //should return 6
+pivot([4,8,2,1,5,7,6,3]) //should return 3
