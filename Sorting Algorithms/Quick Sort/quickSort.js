eb84bfa91ddcccdf9701e2b8bdb28b9a852d5da4
sandond77 <sandu@ucdavis.edu>
2019-11-04 09:40:29 -0800

    added quicSort.js

---
+++ Sorting Algorithms/Quick Sort/quickSort.js
@@ -0,0 +1,35 @@
+function pivot(arr, start=0, end=arr.length+1){
+	let pivot = arr[start];
+	let swapIdx = start;
+	
+	for(let i = start+1; i<arr.length; i++){
+		if(pivot > arr[i]){
+			swapIdx++;
+			swap(arr, swapIdx, i);
+		}
+	}
+	swap(arr, start, swapIdx);
+	return swapIdx
+}
+function swap(arr, index1, index2){
+    let temp = arr[index1];
+    arr[index1] = arr[index2];
+    arr[index2] = temp;
+}
+
+
+function quickSort(arr, left = 0, right = arr.length -1){
+	if(left < right){
+	let pivotIndex = pivot(arr, left, right)
+	//left of pivot
+	quickSort(arr, left, pivotIndex-1);
+	//right of pivot
+	quickSort(arr, pivotIndex+1, right);
+	}
+	console.log(arr);
+	return arr;
+}
+
+
+
+quickSort([4,6,9,1,2,5,3])
