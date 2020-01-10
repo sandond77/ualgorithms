f190f8fca4ab96e91b5b4bda9f85a05ed2de54cc
sandond77 <sandu@ucdavis.edu>
2020-01-10 10:43:31 -0800

    completed radixSort.js

--- Sorting Algorithms/Radix Sort/radixSort.js
+++ Sorting Algorithms/Radix Sort/radixSort.js
@@ -14,3 +14,18 @@ function digitCount(num){
 function getDigit(num, index){
 	return Math.floor(Math.abs(num) / Math.pow(10, index)) % 10;
 }
+
+function radixSort(arr){
+	let maxDigitCount = mostDigit(arr);
+	for(let i=0; i < maxDigitCount; i++){
+		let digitBuckets = Array.from({length: 10}, () => [])
+		for(let j=0; j< arr.length; j++){
+			let digit = getDigit(arr[j],i);
+			digitBuckets[digit].push(arr[j]) //using the current value of comparison digit, we will push the entire number to their respective bucket
+		}
+		arr = [].concat(...digitBuckets) //spread operator allows to to concat all the arrays perfectly without nested arrays happening or creating another loop
+	}
+	return arr
+}
+
+radixSort([125, 12, 12345, 4235, 1346,7])
