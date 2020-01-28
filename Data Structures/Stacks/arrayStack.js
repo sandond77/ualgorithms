2e795339fb673d5a6a701a83ac9b7566b0d2f60f
sandond77 <sandu@ucdavis.edu>
2020-01-28 14:27:13 -0800

    added array stack implementation

---
+++ Data Structures/Stacks/arrayStack.js
@@ -0,0 +1,16 @@
+let stack = [];
+
+stack.push(1);
+stack.push(2);
+stack.push(3);
+
+console.log(stack);
+
+stack.pop();
+console.log(stack)
+
+stack.unshift(0);
+console.log(stack)
+
+stack.shift(0);
+console.log(stack)
