5338da2fb1658ed7560abd212c25d7a15d55a1ae
sandond77 <sandu@ucdavis.edu>
2020-03-08 16:10:37 -0700

    created hashTableClass.js

---
+++ Data Structures/Hash Tables/hashTableClass.js
@@ -0,0 +1,17 @@
+class HashTable {
+	constructor(size=53){
+		this.keyMap = new Array(size)
+	}
+
+	_hash(key) {
+		let total = 0;
+		let WEIRD_PRIME = 0;
+		for(let i=0; i < Math.min(key.length,100); i++){
+			let char = key[i];
+			let value = char.charCodeAt() - 96;
+			total = (total * WEIRD_PRIME + value) % this.keyMap.length;
+		}
+
+		return total;
+	}
+}
