13b6c0f12717508077b449d45c66c78c124ff12b
sandond77 <sandu@ucdavis.edu>
2020-03-24 14:39:36 -0700

    added base file for depth first search recursive on trees

---
+++ Data Structures/Graphs/dfsRecursive.js
@@ -0,0 +1,9 @@
+// Psuedocode
+// DFS(vertex):
+// 	if vertex is empty
+// 		return (this is base case)
+// 	Add vertex to results list
+// 	mark vertex as visited
+// 	for each neightbor in vertex's neighbors:
+// 		if neighbor is not visited:
+// 			recursively call DFS on neightbor
