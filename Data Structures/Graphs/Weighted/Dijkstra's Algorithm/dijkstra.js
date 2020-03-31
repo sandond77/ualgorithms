bf1407bc86c030098de83a5174f10af87006a055
sandond77 <sandu@ucdavis.edu>
2020-03-31 13:04:11 -0700

    finished unopitmized dijkstra algo

--- Data Structures/Graphs/Weighted/Dijkstra's Algorithm/dijkstra.js
+++ Data Structures/Graphs/Weighted/Dijkstra's Algorithm/dijkstra.js
@@ -32,9 +32,60 @@ class WeightedGraph {
 		this.adjacencyList[vertex1].push({node: vertex2, weight});
 		this.adjacencyList[vertex2].push({node: vertex1, weight});
 	}
+
+	dijkstra(start,finish){
+		const nodes = new PriorityQueue();
+		const distances = {};
+		const previous = {};
+		let path = [];
+		let smallest;
+
+		//creates distance object with infinity
+		for(let vertex in this.adjacencyList){
+			if(vertex === start){
+				distances[vertex] = 0;
+				nodes.enqueue(vertex,0);
+			} else {
+				distances[vertex] = Infinity;
+				nodes.enqueue(vertex, Infinity);
+			}
+			previous[vertex] = null;
+		}
+
+		while(nodes.values.length){
+			smallest = nodes.dequeue().val;
+			if(smallest === finish){
+				while(previous[smallest]){
+					path.push(smallest);
+					smallest = previous[smallest];
+				}
+				break;
+			}
+
+			if(smallest || distances[smallest] !== Infinity){
+				for(let neighbor in this.adjacencyList[smallest]){
+					let nextNode = this.adjacencyList[smallest][neighbor]; //finds neighboring nodes
+
+					//calculate the new distance to neighboring node
+					let candidate = distances[smallest] + nextNode.weight;
+					let nextNeighbor = nextNode.node;
+					if(candidate < distances[nextNeighbor]){
+						distances[nextNeighbor] = candidate; //updating new smallest distance to neighbor
+						previous[nextNeighbor] = smallest; //updating previous - how we got to neightbor
+						nodes.enqueue(nextNeighbor, candidate); //enqueue in priority queue with the new priority
+					}
+				}
+			}
+		}
+		console.log(distances);
+		console.log(previous)
+		console.log(path.concat(smallest).reverse());
+		return path.concat(smallest).reverse();
+	}
 }
 
 let graph = new WeightedGraph();
+
 
 graph.addVertex("A");
 graph.addVertex("B");
@@ -51,5 +102,5 @@ graph.addEdge("C","F",4);
 graph.addEdge("D","E",3);
 graph.addEdge("D","F",1);
 graph.addEdge("E","F",1);
-
-console.log(graph)
+graph.dijkstra("A","E");
+// console.log(graph)
